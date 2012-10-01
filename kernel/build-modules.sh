46c5f1d1c80126bb421535a1ee4856dd378cbbb7
ldrifta <ldrifta@gmail.com>
2012-10-01 02:36:53 -0400

    Add OC, Gvnrs & HDMI Fix

---
+++ kernel/build-modules.sh
@@ -0,0 +1,4 @@
+# Edit the path to your repo.
+#
+# Let's build some modules
+ARCH=arm CROSS_COMPILE=~/android/1work/esteem/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- make modules -j4
